88c72d178fa502c9c7acc6c77b4d58ce69134b1c
davethewave90 <david.okerlund@yahoo.com>
2021-01-10 00:41:38 -0700

    fixed last modified

--- time.js
+++ time.js
@@ -1,2 +1,5 @@
-var last_modified = document.lastModified;
+function display_last_modified()
+{
+let last_modified = document.lastModified;
 document.getElementById("time").innerHTML = last_modified;
+}
