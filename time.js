101a01419890006cff855b5b8cc42c95708680ac
davethewave90 <david.okerlund@yahoo.com>
2021-01-13 15:51:18 -0700

    testing

--- time.js
+++ time.js
@@ -1,5 +1,5 @@
-function display_last_modified()
+function display_last_updated()
 {
-let last_modified = document.lastModified;
-document.getElementById("time").innerHTML = last_modified;
+let last_updated = document.lastModified;
+document.getElementById("time").innerHTML = last_updated;
 }
