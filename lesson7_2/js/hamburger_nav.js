f2b5933eb49835345fc91d98b04c42ba7413ae70
davethewave90 <david.okerlund@yahoo.com>
2021-03-09 14:18:05 -0700

    dgf

--- lesson7_2/js/hamburger_nav.js
+++ lesson7_2/js/hamburger_nav.js
@@ -1,5 +1,5 @@
-function toggle_nav()
+function gallery_toggle_nav()
 {
-    document.getElementById("hamburger_nav").classList.toggle("hide_nav");
+    document.getElementById("gallery_hamburger_nav").classList.toggle("hide_nav");
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
