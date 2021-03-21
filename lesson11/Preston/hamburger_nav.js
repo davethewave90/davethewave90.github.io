aa407c0ded122887656176940350fc29291009e8
davethewave90 <david.okerlund@yahoo.com>
2021-03-20 23:18:19 -0600

    hj

---
+++ lesson11/Preston/hamburger_nav.js
@@ -0,0 +1,5 @@
+function toggle_nav()
+{
+    document.getElementById("hamburger_nav").classList.toggle("hide_nav");
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
